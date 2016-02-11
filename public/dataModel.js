5d14e5b8878770230d2d0c6b9ca7cdf864924cfb
Pere Bueno <pere.bueno@gmail.com>
2016-02-11 21:08:48 +0100

    Created data model in a separate node module, loads with mongoose

---
+++ public/dataModel.js
@@ -0,0 +1,20 @@
+// dataModel.js
+// Here is where the data model will be defined
+
+//exports.Todo = {'Todo':{
+//	text : String
+//}};
+
+//var TODO;
+
+//TODO.name = 'Todo';
+//TODO.value={text : String};
+
+//exports.TODO={TODO.name = 'Todo',
+//	value={text : String}
+//};
+
+exports.TODO ={
+	name : "Todo",
+	value : {text : String}
+};
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 
-// SetUp
+// SetUp - external
 var express = require('express');
 var app = express(); 							// Creating express application
 var mongoose = require ('mongoose');			// Including mongoose to create mongo objects
@@ -17,6 +17,10 @@ app.use(bodyParser.urlencoded({'extended':'true'}));	// parse application/x-www-
 app.use(bodyParser.json());						// parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());						// to use our own DELETE & PUT
+
+// App preparation
+var dataModel = require('./public/dataModel.js');
+var Todo = mongoose.model(dataModel.TODO.name, dataModel.TODO.value);
 
 // Start the server, listen in port 8080
 app.listen(8080);
